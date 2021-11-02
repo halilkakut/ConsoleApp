103e229284ad979072dd05c79480aa9083da15ed
HalilKakut <halil.kakut@hotmail.com>
2021-11-02 15:18:41 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsim Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyİsim Giriniz:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + lastName);
+        }
+    }
+}
